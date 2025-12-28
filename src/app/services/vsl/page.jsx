14afb78d8c59ddc7ecb48575d53eb8b76c89ba18
Houss82 <houssem1982mansour@gmail.com>
2025-12-28 23:04:23 +0100

    🔧 Conversion page VSL en Server Component pour améliorer le SEO

--- src/app/services/vsl/page.jsx
+++ src/app/services/vsl/page.jsx
@@ -1,5 +1,4 @@
-"use client";
-
+// Server Component pour meilleur SEO et référencement
 import Footer from "@/components/Footer.jsx";
 import Navbar from "@/components/Navbar.jsx";
 import SEOBreadcrumb from "@/components/SEOBreadcrumb.jsx";
